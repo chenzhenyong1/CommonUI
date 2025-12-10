80e323369946dd4eb8ba719928e27cf9336c1cde
陈振勇 <>
2025-12-10 16:35:19 +0800

    feat: add PrimaryButton

---
+++ Sources/CommonUI/PrimaryButton.swift
@@ -0,0 +1,34 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+
+import SwiftUI
+
+public struct PrimaryButton: View {
+    let title: String
+    let action: () -> Void
+    
+    public init(title: String, action: @escaping () -> Void) {
+        self.title = title
+        self.action = action
+    }
+    
+    public var body: some View {
+        Button(action: action) {
+            Text(title)
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding(.horizontal, 24)
+                .padding(.vertical, 12)
+                .background(Color.blue)
+                .cornerRadius(8)
+        }
+    }
+}
+
+// 必须提供 public initializer
+struct PrimaryButton_Previews: PreviewProvider {
+    static var previews: some View {
+        PrimaryButton(title: "Click Me") {}
+    }
+}
